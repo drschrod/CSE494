d551c81a255587a9953bb968840f92a7d4fd3fcd
drschrod <HallowsEnd@dylanscodersmbp.mobile.asu.edu>
2016-11-07 17:03:44 -0700

    For loop seat constructor
    Added code for seat construction.
    Updated seat.m to include seatNum and row
    Updated SteeringSimulator to have a separate list for seats

--- SteeringSimulator.m
+++ SteeringSimulator.m
@@ -5,6 +5,7 @@ classdef SteeringSimulator
     properties
         fig
         objectList
+        seatList
         time
     end
     
@@ -13,8 +14,8 @@ classdef SteeringSimulator
         function obj=SteeringSimulator()
             obj.fig = figure('units','pixel','Resize','off','position',[50 50 600 500]);
             figure(obj.fig);
-            xlim([0 50])
-            ylim([0 50])
+            xlim([0 100])
+            ylim([0 100])
             
             obj.time = 0;
             
@@ -25,12 +26,26 @@ classdef SteeringSimulator
             obj.objectList{end + 1} = object;
         end
         
+        %%
+        %List that Stores the seats exclusively
+        %Use this for when giving assigned seats and trying to find said
+        %seat
+        function obj = registerSeat(obj, object)
+            obj.seatList{end + 1} = object;
+        end
+        %%
         function visualize(obj)
             figure(obj.fig)
             cla
             hold on
             for i=1:length(obj.objectList)
                 object = obj.objectList{i};
+                object.visualize();
+            end
+            %Added another for loop for the seatList
+            %We should have separate lists for diff object types
+            for i=1:length(obj.seatList)
+                object = obj.seatList{i};
                 object.visualize();
             end
             hold off
--- seat.m
+++ seat.m
@@ -4,6 +4,8 @@ classdef seat < Object
     
     properties
         velocity  % velocity of target (0 for static targets)
+        seatNum   %The seat number within the row
+        row       %The row letter, should be a char a-z
     end
     
     methods
