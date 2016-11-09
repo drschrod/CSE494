a599dccd57d1d930870304021d83e78606b32aa4
drschrod <HallowsEnd@dylanscodersmbp.mobile.asu.edu>
2016-11-09 14:05:14 -0700

    Revert "Lots of changes"
    This reverts commit 191d12b4910826242b2217731c730760b046908f.

--- Steering/Objects/WallRound.m
+++ Steering/Objects/WallRound.m
@@ -15,18 +15,10 @@ classdef WallRound < Object
         
         function visualize(obj)
             v = [obj.position' + [-2 2]; obj.position' + [2 2]]; % this section allows for the walls to be visualized
-            obj.boxTopLeft = obj.position' + [-2 2];  % gives the position of the top left corner of the box
-            obj.boxTopRight =  obj.position' + [2 2]; % gives the position of the top right corner of the box 
-           patch('Faces', 1:2, 'Vertices', v, 'FaceColor', 'red')
-           
-           v = [obj.position' + [2 -2]; obj.position' + [2 2]]; 
-           obj.boxTopRight = obj.position' + [2 2];   %gives position of top right corner of box
-           obj.boxBottomRight = obj.position' + [2 -2]; %gives position of bottom right corner of box
-           patch('Faces', 1:2, 'Vertices', v, 'FaceColor', 'red')
-           
-            v = [obj.position' + [-2 2]; obj.position' + [-2 -2]]; 
-            obj.boxTopLeft = obj.position' + [-2 2]; % gives position of top left corner
-            obj.boxBottomLeft =  obj.position' + [-2 -2]; % gives position of bottom left corner
+            patch('Faces', 1:2, 'Vertices', v, 'FaceColor', 'red')
+            v = [obj.position' + [2 -2]; obj.position' + [2 2]]; 
+            patch('Faces', 1:2, 'Vertices', v, 'FaceColor', 'red')
+             v = [obj.position' + [-2 2]; obj.position' + [-2 -2]]; 
             patch('Faces', 1:2, 'Vertices', v, 'FaceColor', 'red')
         end
            
--- testingstuff.m
+++ testingstuff.m
@@ -9,18 +9,11 @@ sim = SteeringSimulator();
 % For adding targets
 
 s = char(97:122); % Or just use: s = 'abcdefghijklmnopqrstuvwxyz';
-rows=3;
+rows=7;
 
-
-% we'll need to say that the agent cannot pass through the coordinates of
-% topleft corner and top right corner
-% cannot pass through topright corner and bottom right cornerr
-%cannot pass through top left corner and bottom left corner
-%may only pass through the coordinates of bottom left and bottom right
-%corner
 
 for i=1:rows
-    for j=1:3
+    for j=1:7
         chair=seat();
         chair.seatNum=j;
         chair.row=[s(i)];
